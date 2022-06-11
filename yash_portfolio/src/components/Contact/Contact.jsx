bc7c2003839538663826964f7c426598c85aeccc
yash9431-gif <yraj9248@gmail.com>
2022-06-11 19:30:26 +0530

    done

--- yash_portfolio/src/components/Contact/Contact.jsx
+++ yash_portfolio/src/components/Contact/Contact.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import "./Contact.css"
+import phone from "./Images/phone icons.png"
+import email from "./Images/emial.webp"
+ import git from "./Images/git.png"
+ import limk from "./Images/linkedin.png"
 
 const Contact = () => {
   return (
     <div className='contactmain'>
       <div className='contactinfo'>
         <h3>You can find me here</h3>
+        <div className='phone'>
+          <img src={phone} alt="" />
+           <p>---7004383651</p>
+        </div>
+       <div className='phone'>
+          <img src={email} alt="" />
+           <p>---7004383651</p>
+        </div>
+        <a href="https://github.com/yash9431-gif?tab=repositories">
+          <img src={git} alt="" />
+        </a>
+
+         <a href="https://www.linkedin.com/in/yash-raj-926416174/">
+          <img src={limk} alt="" />
+        </a>
+        
+       
       </div>
       
           
--- yash_portfolio/src/components/Project/Project.jsx
+++ yash_portfolio/src/components/Project/Project.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import './Projects.css'
 import homedeput from "./Images/homedeput.png"
 import dineout from "./Images/dineout.png"
+import xoono from "./Images/Xoono.png"
+import git from "./Images/git.png"
+import dep from "./Images/deployed.png"
 
 // import github from "./Images/git.png"
 
@@ -19,29 +22,43 @@ const Project = () => {
             <li>CSS</li>
              <li>JAVA-SCRIPT</li>
           </p>
+            <div className='iconfoot'>
+            <a href="https://github.com/yash9431-gif/HomeDepot"><img src={git} alt="" /></a>
+            <a href="https://trusting-leavitt-137a56.netlify.app/"><img src={dep} alt="" /></a>
+          </div>
         </div>
         
 
           <div className='card1'>
           <h3>DINEOUT ClONE</h3>
           <img src={dineout} alt="" />
-          <p>Its a clone of Dineout App and it provides users with exciting offers on a plethora of restaurants, exclusive discounts on buffets, complimentary dishes & a lot more</p>
+          <p>It provides users with exciting offers on a plethora of restaurants, exclusive discounts on buffets, complimentary dishes & a lot more</p>
           <p>
             <li>HTML</li>
             <li>CSS</li>
-             <li>JAVA-SCRIPT</li>
+            <li>JAVA-SCRIPT</li>
+            
           </p>
+           <div className='iconfoot'>
+            <a href="https://github.com/yash9431-gif/Dinout"><img src={git} alt="" /></a>
+            <a href="https://dinout-zeta.vercel.app/"><img src={dep} alt="" /></a>
+          </div>
         </div>
 
         <div className='card1'>
-          <h3>DINEOUT ClONE</h3>
-          <img src={dineout} alt="" />
-          <p>Its an e-commerce commonly known as Home Depot, is the largest home improvement retailer in the United States</p>
+          <h3>YOOX ClONE</h3>
+          <img src={xoono} alt="" />
+          <p>Yoox is a portal for buying products for both men and women.  Their target audience ranges from kids to adolescents to teens and young. </p>
           <p>
             <li>HTML</li>
             <li>CSS</li>
              <li>JAVA-SCRIPT</li>
           </p>
+           <div className='iconfoot'>
+            <a href="https://github.com/yash9431-gif/Dinout"><img src={git} alt="" /></a>
+            <a href="https://rococo-maamoul-64e2db.netlify.app/"><img src={dep} alt="" /></a>
+          </div>
+   
         </div>
 
         
